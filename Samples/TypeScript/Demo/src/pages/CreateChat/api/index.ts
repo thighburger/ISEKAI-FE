6f20d55a31f62d055758485f7a22718ea6d6abd4
thighnurger <lewis177777gmail.com>
2026-01-02 09:08:31 +0900

    ♻️refactor: 배럴패턴

---
+++ Samples/TypeScript/Demo/src/pages/CreateChat/api/index.ts
@@ -0,0 +1,5 @@
+export { generateCharacter } from './character';
+export type { GenerateCharacterRequest, GenerateCharacterResponse } from './character';
+
+export { generateBackground } from './background';
+export type { GenerateBackgroundRequest, GenerateBackgroundResponse } from './background';
---
+++ Samples/TypeScript/Demo/src/pages/CreateChat/components/index.ts
@@ -0,0 +1,5 @@
+export { NameInput } from './NameInput';
+export { AppearanceInput } from './AppearanceInput';
+export { PersonalityInput } from './PersonalityInput';
+export { VoiceSelector } from './VoiceSelector';
+export { BackgroundInput } from './BackgroundInput';
