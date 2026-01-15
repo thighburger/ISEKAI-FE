9dbd5d510d622136e36da748d07b73daef92e4fb
LeeJS9856 <wnstj1057@naver.com>
2026-01-15 23:31:05 +0900

    🚑fix : api헤더에 로컬 스토리지의 토큰 사용하도록 변경

--- Samples/TypeScript/Demo/src/api/client.ts
+++ Samples/TypeScript/Demo/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
+import { getAccessToken } from '@/utils/kakaoAuth';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || '';
-const AUTH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiaWQiOjIsImVtYWlsIjoibGV3aXMxN0BuYXZlci5jb20iLCJpYXQiOjE3Njc2NzY2MDQsImV4cCI6MTc2NzY5ODIwNH0.gWWvWoUNRRDw0TmdT3VMs1ju7DgWuiPirD83gm_E4Yf9IZXo7jRmjzSLTc7iX6HX6jpFRS5NkmbY8iJKz9tdJQ";
-
+const AUTH_TOKEN = getAccessToken();
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
